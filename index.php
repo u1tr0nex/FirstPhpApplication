14ca85b331a939c7905141fe199fd85127f3e02f
ultronex <mannurawat2010@gmail.com>
2020-10-02 11:20:36 -0400

    First PHP Application

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE>
+<html>
+<head>
+     <title>Abhimanyu Rawat PHP</title>
+</head>
+<body>
+     <h1>Abhimanyu Rawat PHP</h1>
+     <pre>ASCII ART:
+
+       ------    
+      ********   
+     ----------  
+    ****    **** 
+    ------------ 
+    ************ 
+    ----    ---- 
+    ****    **** 
+             
+     </pre>
+     <p>SHA256 hash of "Abhimanyu Rawat" is</p>
+     <?php
+
+     print hash('sha256', 'Abhimanyu Rawat');
+     
+     ?>
+</body>
+</html>
